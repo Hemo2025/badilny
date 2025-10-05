0bb8bcca5cfcd2ec8671b336c3d68f3be9b87faf
Hammam <hemo33272@gmail.com>
2025-10-05 03:09:38 +0300

    sdsd

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.baddil.com',
   appName: 'baddil',
-  webDir: 'build'
+  webDir: 'build',
+  server: {
+    allowNavigation: ['badilny.vercel.app']
+  }
 };
 
 export default config;
